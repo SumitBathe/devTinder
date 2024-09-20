02f3ce25bd928f5d4f6371bf2cac8d07884e2665
sumitbathe <sumitbathe37@gmail.com>
2024-09-20 15:16:37 +0530

    learning routs get post pathch delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require("express");
 const app = express();
 
 
-
-app.use("/test",(req,res)=>{
-    res.send("testing server")
+app.get("/user",(req,res)=>{
+    res.send("Get user sucessfully")
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello")
+app.post("/user",(req,res)=>{
+    //posting data on DB
+    res.send("post user data sucessfully")
 })
-app.use("/home",(req,res)=>{
-    res.send("testing home server")
+
+app.patch("/user",(req,res)=>{
+    res.send("patch data to user sucessfully")
 })
+
+app.delete("/user",(req,res)=>{
+    res.send("delete user sucessfully")
+})
+
+app.use("/test/test1",(req,res)=>{
+    res.send("testing test2 server")
+})  
+
+// while writing routes maintain the order is importent
 
 app.listen(7777,()=>{
     console.log("Server running sucessfully");
