4087aef9817c0ef67d626e34288093247fd5c985
sumitbathe <sumitbathe37@gmail.com>
2024-09-23 17:22:06 +0530

    middleware-authorization-user-admin

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const app = express();
+const {adminAuth, userAuth} = require("./middleware/auth")
 
-app.use("/user",(req,res,next)=>{
-    console.log("request Handler 1");
-    next(); 
-    //This is middleware
-}) 
+app.use("/admin",adminAuth) 
+app.use("/admin",userAuth) 
 
-app.post("/user",(req,res,next)=>{
-    console.log("1 response in post ");
-    next()
-    //this is middleware
-},(req,res)=>{
-    console.log("2nd response in post");
-    res.send("Response Handller")
-    //This is actually a route Handler
+app.get("/admin/getAllData",(req,res,next)=>{
+    console.log("Get the all admin data");   
+    res.send("Get all data")
+})
+app.get("/admin/deleteAdmin",(req,res,next)=>{
+    console.log("Admin deleted");  
+    res.send("Admin deleted") 
+})
+
+app.delete("/user/deleteUser",(req,res,next)=>{
+    console.log("user deleted");  
+    res.send("user deleted") 
+})
+
+app.get("/user/getUser",(req,res,next)=>{
+    console.log("get the user");  
+    res.send("user get") 
+})
+
+app.post("/user/postUser",(req,res,next)=>{
+    console.log("Post the user");  
+    res.send("user Post") 
+})
+
+app.patch("/user/updateUser",(req,res,next)=>{
+    console.log("Put the user");  
+    res.send("user updated") 
 })
 
 app.listen(7777,()=>{
