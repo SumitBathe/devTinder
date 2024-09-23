a1e27ef1d65f727dd078ddb84f1683b5926fae62
sumitbathe <sumitbathe37@gmail.com>
2024-09-23 16:08:27 +0530

    multiple request handler

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 
-
-app.post("/user/:userId/:name",(req,res)=>{
-    //posting data on DB
-    console.log(req.params);    
-    res.send("post user data sucessfully")
-})
-
-// while writing routes maintain the order is importent
+app.use("/user",(req,res,next)=>{
+    console.log("request Handler 1");
+    next();
+    res.send("response 1");
+    
+},[
+(req,res,next)=>{
+    console.log("Request Handler 2");
+    next()
+}],
+(req,res)=>{
+    console.log("Request Handler 3");    
+}
+//we can make a chain of request handler 
+//we can store this chain in array or some request handler in array as well
+)    
 
 app.listen(7777,()=>{
     console.log("Server running sucessfully");
