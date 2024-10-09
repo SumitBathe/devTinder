08a04f239a45e656bcc22c665d4f5b66dde6141b
sumitbathe <sumitbathe37@gmail.com>
2024-10-09 16:01:55 +0530

    change password api created

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const profileRouter = express.Router()
+const validator = require("validator")
+const bcrypt = require("bcrypt")
 
 const {userAuth} = require("../middleware/auth")
 const {validateUpdates} = require("../utils/validation")
@@ -31,6 +33,32 @@ profileRouter.get("/profile",userAuth, async (req,res)=>{
     } catch (error) {
         res.status(400).send("somthing went wrong "+error.message)
     }
+ })
+
+ profileRouter.patch("/changePassword",userAuth, async (req,res)=>{
+    try {
+        const {existingPassword,newPassword} = req.body
+
+    const user = req.user
+    const isPasswordMatch = await user.passwordValidate(existingPassword)
+
+    if(!isPasswordMatch){
+        throw new error("Password not correct")
+    }else{
+        if(!validator.isStrongPassword(newPassword)){
+            throw new Error("Password not valid")
+        }
+        const decryptedPassword = await bcrypt.hash(newPassword,10);  
+        user.password = decryptedPassword 
+
+        await user.save()
+        res.send("Password change sucessfuly...")
+    }
+    } catch (error) {
+        res.status(400).send("somthing went wrong "+error.message)
+    }
+
+
  })
 
 module.exports = profileRouter;
